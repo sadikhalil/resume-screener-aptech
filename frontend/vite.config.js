b030aebfb3744b70fbf2582fb95684db1b90b75a
sadikhalil <166375815+sadikhalil@users.noreply.github.com>
2026-05-19 06:30:53 +0500

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,  // increase limit to stop warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+          axios: ['axios'],
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
